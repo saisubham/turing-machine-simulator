18cb4b8fab7b0c811a1a4b3ad100efba98a2c288
anon <anon@anon.anon>
2023-06-21 15:44:46 +0200

    added alternative display mode

--- turing_machine.h
+++ turing_machine.h
@@ -20,16 +20,26 @@ class TuringMachine {
 				tape;		// input tape on which turing machine works
 	int num_states,	// number of states
 		num_alphs, 	// number of alphabets
-		ptr;		// location of pointer head of turing machine
+		ptr,		// location of pointer head of turing machine
+		present_state_id = 0,
+		read_char_id = 0,
+		dir_id = 0;
 	std::map<std::string, int> state_id;		// maps each state to a unique key
 	std::map<char, int> alph_id;				// maps each alphabet to a unique key
 	std::vector<int> accept_state;
 	std::vector<std::vector<int>> state_table;	// transition table storing next state
-												// for a given (present state, input char) pair
+												// for a given (state, input char) pair
 	std::vector<std::vector<int>> dir_table;	// transition table storing next direction
-												// for a given (state, char) pair
+												// for a given (state, input char) pair
 	std::vector<std::vector<char>> write_table;	// transition table storing next write char
-												// for a given (state, char) pair
+												// for a given (state, input char) pair
+	enum class TapeDisplayMode {
+		SINGLE_LINE,
+		DRAW
+	} tape_display_mode = TapeDisplayMode::SINGLE_LINE;
+
+	std::string state_id_to_name(const int &id);
+	char char_id_to_symbol(const int &id);
 public:
 	TuringMachine(std::string _file_name):
 		file_name(_file_name),
